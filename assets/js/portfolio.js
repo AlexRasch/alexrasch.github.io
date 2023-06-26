a5da505b63a6ba6e89ab296701068095c1d0d063
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-06-26 22:30:18 +0200

    Work in progress, not perfect yet

---
+++ assets/js/portfolio.js
@@ -0,0 +1,35 @@
+    // Filtering portfolio cards
+    document.addEventListener('DOMContentLoaded', function() {
+        const filterLinks = document.querySelectorAll('#navbarFilter .nav-link');
+        const portfolioCards = document.querySelectorAll('#portfolio .card');
+
+        // Add click event listener filter link
+        filterLinks.forEach(function(link) {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                
+                // Get the filter value
+                const filter = this.getAttribute('data-filter');
+
+                // Set active 
+                filterLinks.forEach(function(link) {
+                    link.classList.remove('active');
+                });
+                this.classList.add('active');
+
+                //console.log('Filter:' + filter);
+
+                // Show/hide portfolio cards based on the selected filter
+                portfolioCards.forEach(function(card) {
+                    const cardFilters = card.parentElement.classList;
+                    //console.log('Card Classlist: ' + card.classList);
+                    if (filter === 'all' || cardFilters.contains(filter)) {
+                        card.parentElement.style.display = 'block';
+                    } else {
+                        card.parentElement.style.display = 'none';
+                    }
+                });
+            });
+        });
+    });
+
