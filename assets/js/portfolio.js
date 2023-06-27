cf640a24e2a1717d884f6626363b45c91d2451de
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-06-27 23:09:34 +0200

    added more content and social icons

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,35 +1,37 @@
-    // Filtering portfolio cards
-    document.addEventListener('DOMContentLoaded', function() {
-        const filterLinks = document.querySelectorAll('#navbarFilter .nav-link');
-        const portfolioCards = document.querySelectorAll('#portfolio .card');
+// Filtering portfolio cards
+document.addEventListener('DOMContentLoaded', function () {
+    const filterLinks = document.querySelectorAll('#navbarFilter .nav-link');
+    const portfolioCards = document.querySelectorAll('#portfolio .card');
 
-        // Add click event listener filter link
-        filterLinks.forEach(function(link) {
-            link.addEventListener('click', function(e) {
-                e.preventDefault();
-                
-                // Get the filter value
-                const filter = this.getAttribute('data-filter');
+    // Add click event listener filter link
+    filterLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
 
-                // Set active 
-                filterLinks.forEach(function(link) {
-                    link.classList.remove('active');
-                });
-                this.classList.add('active');
+            // Get the filter value
+            const filter = this.getAttribute('data-filter');
 
-                //console.log('Filter:' + filter);
+            // Set active 
+            filterLinks.forEach(function (link) {
+                link.classList.remove('active');
+            });
+            this.classList.add('active');
 
-                // Show/hide portfolio cards based on the selected filter
-                portfolioCards.forEach(function(card) {
-                    const cardFilters = card.parentElement.classList;
-                    //console.log('Card Classlist: ' + card.classList);
-                    if (filter === 'all' || cardFilters.contains(filter)) {
-                        card.parentElement.style.display = 'block';
-                    } else {
-                        card.parentElement.style.display = 'none';
-                    }
-                });
+            //console.log('Filter:' + filter);
+
+            // Show/hide portfolio cards based on the selected filter
+            portfolioCards.forEach(function (card) {
+                const cardFilters = card.parentElement.classList;
+                //console.log('Card Classlist: ' + card.classList);
+                if (filter === 'all' || cardFilters.contains(filter)) {
+                    card.parentElement.style.display = 'block';
+                } else {
+                    card.parentElement.style.display = 'none';
+                }
             });
         });
     });
+});
+
+// Back to top
 
