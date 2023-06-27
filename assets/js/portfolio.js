ba4c2663d903714b852ef6bb1c889e4d68119e85
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-06-27 23:31:02 +0200

    added back to top button

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,37 +1,47 @@
-// Filtering portfolio cards
 document.addEventListener('DOMContentLoaded', function () {
     const filterLinks = document.querySelectorAll('#navbarFilter .nav-link');
     const portfolioCards = document.querySelectorAll('#portfolio .card');
-
-    // Add click event listener filter link
+    const btnBackToTop = document.querySelectorAll('#btnBackToTop');
+  
     filterLinks.forEach(function (link) {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            // Get the filter value
-            const filter = this.getAttribute('data-filter');
-
-            // Set active 
-            filterLinks.forEach(function (link) {
-                link.classList.remove('active');
-            });
-            this.classList.add('active');
-
-            //console.log('Filter:' + filter);
-
-            // Show/hide portfolio cards based on the selected filter
-            portfolioCards.forEach(function (card) {
-                const cardFilters = card.parentElement.classList;
-                //console.log('Card Classlist: ' + card.classList);
-                if (filter === 'all' || cardFilters.contains(filter)) {
-                    card.parentElement.style.display = 'block';
-                } else {
-                    card.parentElement.style.display = 'none';
-                }
-            });
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        const filter = this.getAttribute('data-filter');
+  
+        filterLinks.forEach(function (link) {
+          link.classList.toggle('active', link === this);
+        }, this);
+  
+        portfolioCards.forEach(function (card) {
+          const cardFilters = card.parentElement.classList;
+          card.parentElement.style.display = (filter === 'all' || cardFilters.contains(filter)) ? 'block' : 'none';
         });
+      });
     });
-});
-
-// Back to top
-
+  
+    window.onscroll = debounce(handleScroll, 100);
+  
+    // ToDo
+    btnBackToTop.forEach(function (button) {
+      button.addEventListener('click', function (event) {
+        event.preventDefault();
+        document.documentElement.scrollTop = 0;
+      });
+    });
+  });
+  
+  function handleScroll() {
+    const btnBackToTop = document.getElementById('btnBackToTop');
+    btnBackToTop.style.display = (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) ? 'block' : 'none';
+  }
+  
+  // Best thing since sliced bread
+  function debounce(func, delay) {
+    let timeoutId;
+    return function () {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(func, delay);
+    };
+  }
+  
